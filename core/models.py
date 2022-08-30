eecc88c277843d1e697fd8c53847f655fa82f6cc
Alisson <alissongf16@gmail.com>
2022-08-30 14:38:05 -0300

    qualquer coisa

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Curso(models.Model):
-    titulo = models.CharField('Título', max_length=100)
+    titulo = models.CharField('Profissão', max_length=100)
     vagas = models.IntegerField('Vagas')
