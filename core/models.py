45179c5fc9643578fc3178e5cfac6d26c40e9bd8
Alisson <alissongf16@gmail.com>
2022-08-30 15:08:02 -0300

    atualizando

--- core/models.py
+++ core/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-
+####carneiro ###
 class Curso(models.Model):
-<<<<<<< HEAD
     titulo = models.CharField('Nome:', max_length=100)
     vagas = models.IntegerField('Profissão:')
-=======
     profissão = models.CharField('Profissão', max_length=100)
     vagas = models.IntegerField('Vagas')
->>>>>>> 2e5c034149b2cbe1b54143533e8bfdbb3ebdf5ab
+
