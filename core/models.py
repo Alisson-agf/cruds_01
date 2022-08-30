7dfcb391970128cbef854b2328c402bfcb56d292
Alisson <alissongf16@gmail.com>
2022-08-30 15:11:20 -0300

    atualizaçaoCRT

--- core/models.py
+++ core/models.py
@@ -3,6 +3,4 @@ from django.db import models
 class Curso(models.Model):
     titulo = models.CharField('Nome:', max_length=100)
     vagas = models.IntegerField('Profissão:')
-    profissão = models.CharField('Profissão', max_length=100)
-    vagas = models.IntegerField('Vagas')
 
